a8cda14354f6ae426128b40559cda6c83e5b1b2d
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-02-20 17:36:34 +0300

    code refactor

--- src/js/model.js
+++ src/js/model.js
@@ -5,7 +5,7 @@ export const state = {
 export const loadRecipe = async function (id) {
   try {
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
