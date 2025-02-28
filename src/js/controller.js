60f0d6e33e42b23e5d3c24b8796d6ee86505b7ea
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-02-28 18:14:38 +0300

    modified

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js';
-import 'regenerator-runtime';
+import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 // recipeView.init(recipeContainer);
 
+if (module.hot) {
+  module.hot.accept();
+}
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -14,7 +18,6 @@ import { async } from 'regenerator-runtime';
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -25,21 +28,28 @@ const controlRecipes = async function () {
     //Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    recipeView.renderError('we could not get the recipe 😊');
+    recipeView.renderError();
   }
 };
 
 const controlSearchResults = async function () {
   try {
-    await model.loadSearchResults('pizza');
-    console.log(model.state.search.results);
+    resultsView.renderSpinner();
+    //Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //Load search results
+    await model.loadSearchResults(query);
+    // Render results
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
 };
-controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
