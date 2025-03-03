9a8c1df6274221ea5d05b22638ee7c3a92462684
vincentkyalomusembi <vincentkyalo822@gmail.com>
2025-03-03 16:57:32 +0300

    implement pagination

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,13 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    // page 1, and there are other pages
+    //page 1, and there are no other pages
+    //last page
+    //other page
+  }
+}
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,11 +1,11 @@
 class searchView {
   _parentEl = document.querySelector('.search');
   getQuery() {
-    return this._parentEl.querySelector('search__field').value;
+    return this._parentEl.querySelector('.search__field').value;
   }
 
   clearInput() {
-    this._parentEl.querySelector('search__field').value = '';
+    this._parentEl.querySelector('.search__field').value = '';
   }
   addHandlerSearch(handler) {
     this._parentEl.addEventListener('submit', function (e) {
